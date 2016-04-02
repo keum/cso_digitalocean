3f78e31318ee50fe86195d71b5759867a78180a1
Fred Lott <lott.fred@gmail.com>
2016-04-01 17:59:02 -0700

    Status for 04-01-2016 05:50:02 PM

--- update_cso_status.sh
+++ update_cso_status.sh
@@ -8,6 +8,6 @@ cd /home/fred/github/cso_digitalocean
 timestamp=$(<timestamp.txt)
 
 # push update to git
-git add -A && \
+git add timestamp.txt cso_test_file.geojson && \
 git commit -m "Status for $timestamp" && \
 git push origin master
