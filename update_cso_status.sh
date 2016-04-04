f949de3d58226f47826f7e29f32c5ba833e2148c
flott <lott.fred@gmail.com>
2016-04-04 15:43:03 +0000

    adjusted for c9

--- update_cso_status.sh
+++ update_cso_status.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-source /home/fred/.bash_profile
+# source /home/fred/.bash_profile
 
 # change this to your path
-cd /home/fred/github/cso_digitalocean
+cd /home/ubuntu/workspace/cso_digitalocean
 ./cso_status_geojson.py
 
 # grab timestamp from file
 timestamp=$(<timestamp.txt)
 
 # push update to git
-git add .
+git add -A
 git commit -m "Status for $timestamp"
 git push origin master
